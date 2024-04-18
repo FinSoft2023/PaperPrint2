e444ff858534cc02e4101d857ec4894d650597e1
Teerachai Laothong <teerachai@finsoft2023.com>
2024-04-18 16:53:02 +0700

    Handle number when print

--- src/apps/frontface/components/FiLL.vue
+++ src/apps/frontface/components/FiLL.vue
@@ -9,6 +9,10 @@ const props = defineProps<{
 
 // Computed property to format the date if it is a valid date string
 const formattedData = computed(() => {
+  if (typeof props.data == 'number') {
+    console.log('number', props.data);    
+    return props.data.toLocaleString();
+  }
   if (Date.parse(props.data)) {
     return formatDate(props.data);
   }
@@ -19,6 +23,6 @@ const formattedData = computed(() => {
 const formatDate = (date: string) => {
   const formattedDate = new Date(date);
   const bcYear = formattedDate.getFullYear() + 543;
-  return formattedDate.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }).replace(/(\d+)\/(\d+)\/(\d+)/, '$1/$2/' + bcYear);
+  return formattedDate.toLocaleDateString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }).replace(/(\d+)\/(\d+)\/(\d+)/, '$1/$2/ พ.ศ. ' + bcYear);
 }
 </script>
--- src/apps/frontface/server/api/sampleWithData.ts
+++ src/apps/frontface/server/api/sampleWithData.ts
@@ -2,5 +2,6 @@ export default defineEventHandler(async (event) => {
   return {
     receivedLocation: "สหกรณ์ออมทรัพย์ รพ. ศรีสะเกษ",
     dateReceived: new Date(),
+    monthlyIncome: 10000,
   }
 });

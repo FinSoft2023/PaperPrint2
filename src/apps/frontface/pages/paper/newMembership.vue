9aeb0744deb80c9aadbf2b9a3a22e36c45ade159
reregami Nonthawat <66309010023@loeitech.ac.th>
2024-04-19 15:47:55 +0700

    add page 5 #41

--- src/apps/frontface/pages/paper/newMembership.vue
+++ src/apps/frontface/pages/paper/newMembership.vue
@@ -85,7 +85,31 @@
             <!-- สมัครสมาชิกใหม่หน้า apply4 -->
         </MorePaperDoc>
         <MorePaperDoc>
-            <!-- สมัครสมาชิกใหม่หน้า apply5 -->
+            <div class="flex flex-col">
+        <p class="indent-20 text-start ml-10">ข้อ 8 ข้าพเจ้าสัญญาว่า  ถ้าคณะผู้จัดจัดตั้งสหกรณ์ตกลงให้ข้าพเจ้าเป็นสมาชิกได้ และเมื่อได้</p>
+        <p class="text-start ml-10"> จดทะเบียนสหกรณ์ฯ ขึ้นแล้ว ข้าพเจ้าจะลงลายมือชื่อในทะเบียนสมาชิก ทั้งชำระค่าธรรมเนียมแรกเข้าและ</p>
+        <p class="text-start ml-10">เงินดังกล่าว ข้าพเจ้ายินยอมขอร้องให้ปฏิบัติตามความในข้อ 7 ด้วย</p>
+        <p class="indent-20 text-start ml-10">ข้อ 9. ถ้าข้าพเจ้าได้เป็นสมาชิก จะปฏิบัติตามข้อบังคับ ระเบียบ และมติของสหกรณ์ฯ</p>
+        <br>
+        <p class="text-start ml-10">ทุกประการ</p>
+        </div>
+  
+        <div class="grid grid-cols-2 justify-center mt-3">
+<div class="flex items-start">
+
+</div>
+
+
+<div class="">
+
+  <p class="mt-4 text-right mr-20">..............................................................</p>
+  <p class="mt-4 text-right mr-20">(...........................................................)</p>
+  <p class="mt-4 text-right mr-40">ผู้สมัคร</p>
+  
+
+</div>
+
+</div>
         </MorePaperDoc>
         <MorePaperDoc>
             <!-- สมัครสมาชิกใหม่หน้า apply6 -->

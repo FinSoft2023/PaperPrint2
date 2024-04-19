fea600243c7db7d26bd27240d2eff27aacd6024e
SkySuriya <suriya.p@loeitech.ac.th>
2024-04-19 15:02:54 +0700

    update ts #17

---
+++ src/apps/frontface/server/api/loanEmergency.ts
@@ -0,0 +1,13 @@
+export default defineEventHandler(async (event) => {
+    return {
+      receivedLocation: "สหกรณ์ออมทรัพย์ รพ. ศรีสะเกษ",
+      dateReceived: new Date(),
+      monthlyIncome: 10000,
+      loanAccountNumber: "345/67",
+      repaymentPlan: [
+        { m: "Jan", amount: 1000 },
+        { m: "Feb", amount: 1000 },
+        { m: "March", amount: 1000 },
+      ],
+    }
+  });

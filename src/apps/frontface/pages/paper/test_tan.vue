0a639092d8174b003750a391f36f9cfd098c1e8d
reregami Nonthawat <66309010023@loeitech.ac.th>
2024-04-19 15:18:04 +0700

    up_tan

---
+++ src/apps/frontface/pages/paper/test_tan.vue
@@ -0,0 +1,256 @@
+<template>
+    <main>
+      <PaperDoc>
+        <div class="flex flex-col">
+        <p class="indent-20 text-start ml-10">ข้อ 8 ข้าพเจ้าสัญญาว่า  ถ้าคณะผู้จัดจัดตั้งสหกรณ์ตกลงให้ข้าพเจ้าเป็นสมาชิกได้ และเมื่อได้</p>
+        <p class="text-start ml-10"> จดทะเบียนสหกรณ์ฯ ขึ้นแล้ว ข้าพเจ้าจะลงลายมือชื่อในทะเบียนสมาชิก ทั้งชำระค่าธรรมเนียมแรกเข้าและ</p>
+        <p class="text-start ml-10">เงินดังกล่าว ข้าพเจ้ายินยอมขอร้องให้ปฏิบัติตามความในข้อ 7 ด้วย</p>
+        <p class="indent-20 text-start ml-10">ข้อ 9. ถ้าข้าพเจ้าได้เป็นสมาชิก จะปฏิบัติตามข้อบังคับ ระเบียบ และมติของสหกรณ์ฯ</p>
+        <br>
+        <p class="text-start ml-10">ทุกประการ</p>
+        </div>
+  
+        <div class="grid grid-cols-2 justify-center mt-3">
+<div class="flex items-start">
+
+</div>
+
+
+<div class="">
+
+  <p class="mt-4 text-right mr-20">..............................................................</p>
+  <p class="mt-4 text-right mr-20">(...........................................................)</p>
+  <p class="mt-4 text-right mr-40">ผู้สมัคร</p>
+  
+
+</div>
+
+</div>
+      </PaperDoc>
+  
+      <MorePaperDoc>
+        <div class="border-t border-r border-l border-b border-gray-900 p-2">
+          <div class="flex justify-center font-bold text-xl mt-2">(สำหรับเจ้าหน้าที่ของสหกรณ์)</div>
+          <div class="flex justify-center mt-3">
+            <div>
+              <p>จำนวนเงินกู้..................................................................บาท</p>
+  
+            </div>
+          </div>
+  
+          <div class="border-t border-r border-l border-b border-gray-900 p-2 mt-3">
+            <div class="flex justify-center font-bold text-xl">จำกัดวงเงินเพื่อเหตุฉุกเฉิน</div>
+  
+          </div>
+  
+          <table class="w-full text-xs text-left text-gray-500 dark:text-gray-400 border border-black">
+  
+            <tbody>
+  
+  
+              <tr class="bg-white dark:bg-gray-800 ">
+                <td scope="row"
+                  class="px-6 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                  เงินได้รายเดือน
+                </td>
+  
+                <td
+                  class="px-6 py-1 font-bold text-xs text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+  
+                  <p>ต้นเงินกู้สามัญ</p>
+                  <p class="text-center"> คงเหลือ</p>
+                </td>
+                <td scope="row"
+                  class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                  <p>ต้นเงินกู้เพื่อเหตุฉุกเฉิน</p>
+                  <p class="text-center"> คงเหลือ</p>
+  
+                </td>
+                <td scope="row"
+                  class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                  เงินได้รายเดือน
+                </td>
+                <td scope="row"
+                  class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                  เงินได้รายเดือน
+                </td>
+              </tr>
+  
+              <tr class="bg-white dark:bg-gray-800">
+                <th scope="row"
+                  class="px-6 py-8 font-medium text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+  
+                </th>
+  
+                <td class="px-6 py-8 border-b border-r border-black ">
+  
+                </td>
+                <td class="px-6 py-8 border-b border-r border-black ">
+  
+                </td>
+                <td class="px-6 py-8 border-b border-r border-black ">
+  
+                </td>
+                <td class="px-6 py-8 border-b border-r border-black ">
+  
+                </td>
+  
+              </tr>
+  
+  
+  
+  
+  
+  
+            </tbody>
+  
+          </table>
+          <div class="flex text-md mt-2">
+            <div class="font-bold">หมายเหตุ
+            </div>
+            <div class="ml-8">๑. ผู้ขอกู้ เคยผิดนัดการส่งเงินงวดชำระหนี้ หรือขาดส่งเงินค่าหุ้นรายเดือนหรือไม่
+            </div>
+          </div>
+  
+  
+          <div class="flex justify-center mt-3">
+  
+            <p class="ml-2 ">
+  
+            <p class="border-t border-r border-l border-b border-gray-900 px-3 "></p>
+            <p class="mr-16">เคย</p>
+  
+            <p class="border-t border-r border-l border-b border-gray-900 px-3"></p>
+            <p>ไม่เคย</p>
+  
+            </p>
+  
+          </div>
+  
+          <div class="text-md mt-2">
+  
+            <div class="ml-20">๒.
+              ข้อชี้แจงอื่นๆ....................................................................................................................
+            </div>
+          </div>
+  
+          <div class="flex justify-center mt-3">
+  
+            <p class="ml-5">
+            <p class="border-t border-r border-l border-b border-gray-900 px-3 "></p>
+            <p class="mr-12">อนุมัติ</p>
+  
+            <p class="border-t border-r border-l border-b border-gray-900 px-3"></p>
+            <p>ไม่อนุมัติ</p>
+  
+            </p>
+  
+          </div>
+          <p class="indent-20 break-all mt-3">
+            ............................................................................................................................เจ้าหน้าที่
+  
+          </p>
+  
+  
+          <div class="flex justify-center mt-3">
+  
+            <p class="ml-5">
+            <p class="border-t border-r border-l border-b border-gray-900 px-3 "></p>
+            <p class="mr-12">อนุมัติ</p>
+  
+            <p class="border-t border-r border-l border-b border-gray-900 px-3"></p>
+            <p>ไม่อนุมัติ</p>
+  
+            </p>
+  
+          </div>
+          <p class="indent-20 break-all mt-3">
+            ................................................................................ประธาน / รองประธาน /
+            ผู้จัดการ / ผู้ที่ได้
+            รับมอบหมาย
+  
+          </p>
+  
+  
+  
+  
+  
+        </div>
+        <div class="mt-3 px-2">
+  
+  
+          <p class="indent-20 break-all ">ข้าพเจ้าผู้กู้มอบอำนาจให้
+            ..................................................................................ตำแหน่ง.........................................
+          </p>
+  
+          <p class="">
+            สังกัด................................................................เป็นผู้รับเงินกู้ตามหนังสือกู้แทนข้าพเจ้า
+          </p>
+  
+        </div>
+  
+        <div class="grid grid-cols-2 justify-center mt-3">
+          <div class="flex items-start">
+            <div class="border-2 border-black p-2">
+              <p class="underline text-center">คำเตือน</p>
+              <p class="text-center">ถ้าท่านมอบอำนาจให้ผู้อื่นรับเงินแทน</p>
+              <p class="text-center">โปรดฝากบัตรประจำของท่านไปกับผู้รับเงิน</p>
+              <p class="text-center">แทนท่านด้วยเพื่อแสดงต่อเจ้าหน้าที่</p>
+            </div>
+          </div>
+  
+  
+          <div class="">
+  
+  
+            <p>................................................................ผู้กู้</p>
+            <p>................................................................ผู้รับมอบอำนาจ</p>
+  
+            <p class="mt-4">................................................................พยาน</p>
+            <p>(................................................................)</p>
+  
+            <p class="mt-4">................................................................พยาน</p>
+            <p>(................................................................)</p>
+  
+          </div>
+  
+        </div>
+  
+        <div class="mt-3 px-2">
+  
+  
+          <p class="indent-20 break-all ">ข้าพเจ้า
+            ..................................................................................ได้รับเงินกู้
+            จำนวน.........................................บาท
+          </p>
+  
+          <p class="">
+            (................................................................) ไปเป็นการถูกต้องแล้ว ณ
+            วันที่................/................./...................
+          </p>
+  
+        </div>
+  
+        <div class="grid grid-cols-2 justify-center mt-3">
+          <div class="flex items-start">
+  
+          </div>
+  
+  
+          <div class="">
+  
+            <p class="mt-4">................................................................ผู้รับเงิน</p>
+            <p>(................................................................)</p>
+            <p class="text-xs mr-8">ต้องลงลายมือชื่อในการรับเงินต่อหน้าเจ้าหน้าที่ของสหกรณ์</p>
+  
+          </div>
+  
+        </div>
+        <p class="mt-4 text-right">
+          จ่ายเงินถูกต้องแล้ว........................................................................เจ้าหน้าที่การเงิน
+        </p>
+        <p class="mt-4 text-right mr-20">
+          (....................................................................................)</p>
+      </MorePaperDoc>
+    </main>
+  </template>

e9e6d6d474e1c784f1c75217682b62045fc5e74b
toto017 <66309010017@loeitech.ac.th>
2024-04-19 16:31:08 +0700

    add page 2 #38

--- src/apps/frontface/pages/paper/newMembership.vue
+++ src/apps/frontface/pages/paper/newMembership.vue
@@ -5,7 +5,38 @@
         </PaperDoc>
 
         <MorePaperDoc>
-            <!-- สมัครสมาชิกใหม่หน้า apply2 -->
+            <div class="mx-auto max-w-2xl">
+                <p class="text-center mb-4">ชื่อสมาชิก _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ เลขทะเบียนที่ _ _ _ _ _ _ _ _ _ _ _ หน่วย _ _ _ _ _ _ _ _ _ _ _ _ </p>
+                <hr class="my-4 border-b-2 border-black">
+                <p class="text-center mb-4">บัตรตัวอย่างลายมือชื่อสมาชิก</p>
+                <hr class="my-1 border-b-1 border-black">
+                <hr class="my-1 border-b-1 border-black">
+                
+                <div class="flex justify-between text-center">
+                    <div class="flex ">
+                        <p class="ml-20 text-center mt-3 ml-50">ตัวอย่างลายมือชื่อ</p>
+                    </div>
+                    <div class="flex items-center">
+                        <p class="mr-20 text-center mt-3">วันที่ _ _ _ _ /_ _ _ _ /_ _ _ _</p>
+                    </div>
+                </div>
+                
+                <hr class="my-1 border-b-1 border-black">
+                <hr class="my-1 border-b-1 border-black">
+                <ol class="list-decimal mx-auto max-w-xs">
+                    <li class="text-center mb-2 mt-6"> _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _</li>
+                    <li class="text-center mb-2"> _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _</li>
+                    <li class="text-center mb-3"> _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _</li>
+                </ol>
+                <div>
+                    <p class="text-center mb-1">(โปรดลงลายมือชื่อคนละ 3 ลายมือชื่อ)</p>
+                    <p class="text-center mb-4">ขอรับรองว่าลายมือชื่อข้างบนนี้ เป็นลายมือชื่อของสมาชิกผู้นี้จริง</p>
+                    <p class="text-center mb-4 ml-19">_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ พยาน</p>
+                    <p class="text-center mb-4 mr-9">(_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _)</p>
+                    <p class="text-center mb-4 ml-19">_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ พยาน</p>
+                    <p class="text-center mb-4 mr-9">(_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _)</p>
+                </div>
+            </div>
         </MorePaperDoc>
 
         <MorePaperDoc>

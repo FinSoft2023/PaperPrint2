4927fced287522b2be3ed5b0fb51e03cd984276d
Teerachai Laothong <teerachai@finsoft2023.com>
2024-04-22 14:16:58 +0700

    Added FiLLBox, BlankSpace and makes FiLL underlined.

---
+++ src/apps/frontface/components/DynamicIconsList.vue
@@ -0,0 +1,6 @@
+<template>
+  <UIcon name="i-mdi-checkbox-marked-outline"
+    class="inline-block align-middle" />
+  <UIcon name="i-mdi-checkbox-blank-outline"
+    class="inline-block align-middle" />
+</template>

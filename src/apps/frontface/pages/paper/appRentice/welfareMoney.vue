0940da60aa23c0edd1a5af20e65c2fb989db67df
kanyarat20 <66309010020@loeitech.ac.th>
2024-04-26 12:46:35 +0700

    update #5

--- src/apps/frontface/pages/paper/appRentice/welfareMoney.vue
+++ src/apps/frontface/pages/paper/appRentice/welfareMoney.vue
@@ -1,7 +1,18 @@
 <template>
-    <main class="font-sans">
+    <main >
         <PaperDoc>
-
+            <br><br>  <br><br>
+            <div class="flex text-cenetr">
+            <div class="items-start ">
+                <img class="w-[70px] h-auto"
+                  src="https://www.saving-sskh.com/images/logo-saving.png"
+                  alt="Logo" />
+                  </div>
+              <p class="text-3xl flex pl-56"> บันทึกข้อความ </p>
+            </div>
+            <div class="flex">
+<p class="text-lg ">ส่วนราชการ</p> <p class="text-base ">สหกรณ์ออมทรัพย์โรงพยาบาลศรีสะเกษ จำกัด</p>
+</div>
             
         </PaperDoc>
 

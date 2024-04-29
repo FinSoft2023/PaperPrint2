ecbd243eddf6ed77a82cbe1d1f0dc99c02446caf
Beam-1 <66309010022@loeitech.ac.th>
2024-04-29 12:07:37 +0700

    updata #14

--- src/apps/frontface/pages/paper/appRentice/prepare3.vue
+++ src/apps/frontface/pages/paper/appRentice/prepare3.vue
@@ -1,23 +1,150 @@
 <template>
     <main class="font-sans">
-      <PaperDoc>
+      <MorePaperDoc>
                         <!-- กู้สามัญทั่วไป general1  -->
 
-        </PaperDoc>
-        <PaperDoc>
+                        <br><br> 
+            <div class="border-2 border-slate-950">
+              <p class="underline underline-offset-1 text-lg font-semibold text-center "> ขั้นตอนการสมัครสมาชิก สส.ชสอ. ประเภทสามัญ</p>
+              <p class="underline underline-offset-1 text-lg font-semibold text-center "> ผู้สมัครต้องเป็นสมาชิกสหกรณ์มาแล้วไม่น้อยกว่า 1 ปี (เริ่มใช้มกราคม 2566 เป็นต้นไป)</p>
+              <p class="text-base">1.กรอกใบสมัครให้เรียบร้อย (ด้วยลายมือที่อ่านง่ายและชัดเจน)</p>
+              <p class="text-base">2.ขอใบรับรองแพทย์จากโรงพยาบาลของรัฐ (ใช้แบบฟอร์มของ รพ.รัฐ เท่านั้น)</p>
+              <p class="text-base">3.เตรียมเอกสารประกอบการสมัคร ดังนี้</p>
+              <p class="text-base ml-8">3.1 สำเนาบัตรประชาชนผู้สมัครและผู้รับผลประโยชน์ อย่างละ 1 ฉบับ</p>
+              <p class="text-base ml-8">3.2 สำเนาทะเบียนบ้านผู้สมัครและผู้รับผลประโยชน์ อย่างละ 1 ฉบับ</p>
+              <p class="text-base ml-8">3.3 สำเนาทะเบียนสมรส หรือสำเนาทะเบียนหย่า ของผู้สมัคร (ถ้ามี) จำนวน 1 ฉบับ</p>
+              <p class="text-base ml-8">3.4 สำเนาใบเปลี่ยนชื่อ / สกุล ของผู้สมัครและผู้รับผลประโยชน์ (ถ้ามี) อย่างละ 1 ฉบับ</p>
+              <p class="text-base">4.ผู้รับผลประโยชน์ต้องลงชื่อในส่วนผู้รับผลประโยชน์ และ แบบคำรับรองสุขภาพตนเอง</p>
+              <p class="text-base">5.ผู้สมัครต้องมาสมัครด้วยตนเอง เพื่อถ่ายรูปเต็มตัว ณ วันสมัคร แนบเป็นหลักฐาน</p>
+              <p class="text-base">6.ชำระค่าสมัครและเงินสงเคราะห์ล่วงหน้า ด้วยการโอนเข้าบัญชีธนาคารของสหกรณ์ฯ
+                <br> จำนวน 4,840 .- บาท
+              </p>
+              <p class="underline underline-offset-1 text-base font-semibold  "> หมายเหตุ เพื่อความสะดวกรวดเร็ว รบกวนแจ้งเจ้าหน้าที่เพื่อนัดวัน เวลา ในการสมัคร</p>
+              <p class="text-base mt-1">โทร.0943934878 สุภานัน</p>
+
+
+            </div>
+
+        </MorePaperDoc>
+        <MorePaperDoc>
                         <!-- กู้สามัญทั่วไป general2  -->
 
-        </PaperDoc>
+                        <div class="grid grid-cols-3 grid-rows-2 justify-center h-10">
+              <div class="items-start mt-4">
+                <div class="border-2 border-black p-1 ">
+                  <p class="text-xs font-semibold tracking-tighter">โปรดเขียนตัวบรรจงและกรอกข้อมูลให้ครบทุกช่อง
+                  </p>
+                </div>
+                <div class="border-2 border-black p-1 ">
+
+                  <p class=" text-xs font-semibold">เอกสารประกอบ
+                   <p class="text-xs font-semibold" > <FiLLBox /> 1. สำเนาบัตรประชาชน หรือบัตรข้าราชการผู้สมัคร</p>
+                   <p class="text-xs font-semibold" > <FiLLBox /> 2. สำเนาทะเบียนบ้านผู้สมัคร</p>
+                   <p class="text-xs font-semibold" > <FiLLBox /> 3. ใบรับรองเเพทย์ของรัฐฉบับจริง มีอายุไม่เกิน 30 วัน</p>
+                   <p class="text-xs font-semibold" > <FiLLBox /> 4. ใบรับรองสุขภาพตนเอง</p>
+                   <p class="text-xs font-semibold" > <FiLLBox /> 5. สำเนาบัตรปรชาชน เเละทะเบียนบ้านผู้รับเงินสังเคราะห์</p>
+                   <p class="text-xs font-semibold" > <FiLLBox /> 6. รูปถ่าย ณ วันยื่นเอกสาร</p>
+                   <p class="text-xs font-semibold" > <FiLLBox /> 7. หนังสือรับรองการเป็นสมาชิกประเภทสามัญของ สอ.</p>
+                   <p class="text-xs font-semibold" > <FiLLBox /> 8. สำเนาทะเบียนสมรส (ถ้ามี)</p>
+                   <p class="text-xs font-semibold" > <FiLLBox /> 9. ใบสำคัญการเปลี่ยนชื่อ-นามสกุล (ถ้ามี)</p>
+
+
+
+                  </p>
+                </div>
+
+             
+                </div>
+
+              <div class="flex flex-col items-center row-span-2 mt-48">
+                          <img class="w-[100px] h-auto"
+                            src="https://cypphcoop.com/mediafiles/data/sorsortortor.png"
+                            alt="Logo" />
+                            
+              </div>
+
+              <div class="items-start mt-2 mr-14">
+                <p class="text-end mr-2  font-semibold">สส.ชสอ. ส.1/1 (สามัญ)</p>
+                <div class="border-2 border-black p-1 mt-4">
+                  <p class="text-xs font-semibold tracking-tighter text-center">สมัครผ่าน
+                  </p>
+                </div>
+                <div class="border-2 border-black p-1 ">
+                <p class="text-xs font-semibold" > <FiLLBox /> ศูนย์ประสารงาน</p>
+                <p class="text-xs font-semibold" > <FiLLBox /> ศูนย์ประสารงานระดับจังหวัด</p>
+
+                </div>
+
+                <div class="border-2 border-black p-1 mt-1">
+                  <p class="text-xs font-semibold tracking-tighter text-center">สำหรับเจ้าหน้าที่สมาคม
+                  </p>
+                </div>
+                <div class="border-2 border-black p-1 ">
+                <p class="text-xs font-semibold" >เลขฌาปนกิจ</p>
+                <p class="text-xs font-semibold" >รอบสมัคร</p>
+
+                </div>
+                </div>
+
+            </div>
+            <br>  <br>  <br>  <br>  <br>  <br>  <br>  <br>
+      <p class="text-lg text-center font-semibold mt-16 "> ใบสมัครสมาชิก <span class="underline underline-offset-2">ประเภทสามัญ</span><br>
+      </p>
+      <p class="text-base text-center font-semibold ">สมาคมฌาปนกิจสงเคราะห์สหกรณ์สมาชิกของชุมนุมสหกรณ์ออมทรัพย์แห่งประเทศไทย (สส.ชสอ.)</p>
+      <div class="mt-4 mr-14">
+      <p class="text-base text-end"> เขียนที่ <FiLL :data="data?.location" class="min-w-80" /></p>
+       <p class="text-base text-end "> วันที่ <FiLL  class="min-w-80 " :data="data?.dateReceived" date-format="full" /></p>
+       <p  class="text-base text-start font-semibold">(1) <span class="underline underline-offset-2">ข้อมูลสมาชิก</span> :</p>
+     <p class="text-base mt-2 "> ข้าพเจ้า (นางสาว/นาง/นาย) 
+              <FiLL class="min-w-80" :data="`${data?.prefix} ${data?.fName}  ${data?.lName}`"/> วัน/เดือน/ปี/ เกิด <FiLL  class="min-w-56 " :data="data?.dateReceived" date-format="full" />
+            อายุ <FiLL :data="data?.age" class="min-w-24" /> ปี เลขประจำตัวประชาชน <FiLL :data="data?.idcard" class="min-w-96" /> เพศ <FiLLBox /> ชาย <FiLLBox /> หญิง
+          </p> 
+          <p>สถานภาพ <FiLLBox /> โสด <FiLLBox /> สมรส คู่สมรสชื่อ <FiLL class="min-w-96" :data="`${data?.newprefix} ${data?.newfName}  ${data?.lName}`"/> <FiLLBox /> หย่า หรือ หม้าย
+            เป็นสมาชิกสามัญของสหกรณ์ออมทรัพย์ <FiLL :data="data?.age" class="min-w-24" /> เลขทะเบียนสมาชิก <FiLL :data="data?.age" class="min-w-24" /> โทรศัพท์มือถือ <FiLL :data="data?.age" class="min-w-24" />
+            โทรศัพท์บ้าน <FiLL :data="data?.age" class="min-w-24" /> โทรศัพท์ที่ทำงาน <FiLL :data="data?.age" class="min-w-24" /> Email <FiLL :data="data?.age" class="min-w-24" />
+          </p>
+          <p  class="text-base text-start font-semibold">(2) <span class="underline underline-offset-2">กลุ่มวิชาชีพของศูนย์ประสานงานดันสังกัย</span> :</p>
+          <p class="ml-6"><FiLLBox /> ครู <FiLLBox /> สาธารณสุข <FiLLBox /> ทหาร <FiLLBox /> ตำรวจ <FiLLBox /> ราชการ <FiLLBox /> รัฐวิสาหกิจ <FiLLBox /> ประกอบการ </p> 
+          <p class="mt-2">สังกัด <FiLL :data="data?.age" class="min-w-24" /> หน่วยงาน <FiLL :data="data?.age" class="min-w-24" /> ดำรงตำเเหน่ง <FiLL :data="data?.age" class="min-w-24" /></p>
+          <p  class="text-base text-start font-semibold">(2) <span class="underline underline-offset-2">กลุ่มวิชาชีพของศูนย์ประสานงานดันสังกัย</span> :</p>
+          <p class="ml-6"><FiLLBox /> ครู <FiLLBox /> สาธารณสุข <FiLLBox /> ทหาร <FiLLBox /> ตำรวจ <FiLLBox /> ราชการ <FiLLBox /> รัฐวิสาหกิจ <FiLLBox /> ประกอบการ </p> 
+          <p class="mt-2">สังกัด <FiLL :data="data?.age" class="min-w-24" /> หน่วยงาน <FiLL :data="data?.age" class="min-w-24" /> ดำรงตำเเหน่ง <FiLL :data="data?.age" class="min-w-24" /></p>
+<div class="grid grid-rows-2 grid-flow-col">
+                <div class="row-span-3">
+                  <p class="ml-12" ><FiLLBox/> ค่าสมัครสมาชิกครั้งแรก</p>
+                  <p class="ml-12"> <FiLLBox/> ค่าบำรุงรายปี </p>
+                 
+                </div>
+                <div class="row-span-2 col-span-2 ml-10">
+                  <p>เป็นเงิน 20 บาท</p>
+                  <p>เป็นเงิน 20 บาท</p>                  
+                </div>
+            </div>
+            <span class="text-base ml-12"><FiLLBox/> เงินสงเคราะห์ล่วงหน้า..............5,500.....บาท</span> <br>
+            <span class="text-base ml-20">รวมชำระหนี้ครั้งนี้ เป็นเงิน..........5,500.....บาท</span> (ห้าพันห้าร้อยยี่สิบบาทถ้วน)<br>
+            <span class="text-base ml-12">4.2 การชำระเงินครั้งต่อไป ข้าพเจ้ายินยอมให้หักและชำระเงินสงเคราะห์ล่วงหน้า ดังน้ี</span><br>
+            <span class="text-base ml-12"> <FiLLBox /> ชำเป็นเงินสดที่สมาคมหรือศุนย์ประสานงานสมาคม</span><br>
+            <span class="text-base ml-12"> <FiLLBox /> ชำระเงินเข้าบัญชีสมาคมธนาคารไทยพาณิชย์ สาขาเดอะคริสตัล ราชพฤกษ์ ชื่อบัญชี สมาคมฌาปนกิจ</span>สงเคราะห์สมาชิกสหกรณ์ออมทรัพย์สาธารณสุขไทย
+            สมาคมฌาปนกิจสงเคราะห์สมาชิกสหกรณ์ออมทรัพย์สาธารณสุขไทย ประเภทกระแสรายวัน (Bill Payment Pay- In Slip) เลขบัญชี 458-0-39888-5<br>
+            <span class="text-base ml-12"> <FiLLBox /> ชำระจากเงินปันผล เฉลี่ยคืน จากสหกรณ์ต้นสังกัด <br>
+              <span class="text-base ml-12"> ข้าพเจ้าได้ทราบและเข้าใจวัตถุประสงค์ </span> ข้อบังคับ ระเบียบ และประกาศของสมาคมฌาปนกิจสงเคราะห์</span>
+            สมาชิกสามัญของสหกรณ์ออมทรัพย์สาธารณสุขไทย แล้ว มีความประสงค์สมัครเป็นสมาชิก และยินยอมปฏิบัติตามข้อบังคับและเงื่อไขดังกล่าวทุกประการ<br>
+ 
+<p class="text-base pl-96 ">ลงชื่อ...........................................................ผู้สมัคร </p>
+<p class="text-base pl-96 ">( <FiLL class="min-w-52" :data="`${data?.prefix} ${data?.fName}  ${data?.lName}`"/> )</p>
+</div>
+
+        </MorePaperDoc>
       <MorePaperDoc>
                       <!-- กู้สามัญทั่วไป general3  -->
-        <div class="grid justify-items-start font-bold mt-12 ml-48">
+        <div class="grid justify-items-start font-bold  ml-48">
           <p class="pl-96">สส.ชสอ. ส.1/2(สามัญ)</p>
         </div>
 
         <div class=" grid justify-items-center mt-6">
             <p class=" font-medium font-bold text-xl">สำหรับเจ้าหน้าที่ศูนย์ประสานงาน</p>
         </div>
-        <div class="grid justify-items-start text-pretty text-base mt-2">
+        <div class="grid justify-items-start text-pretty text-base mt-1">
           <p><span class="ml-11">ตรวจสอบแล้วขอรับรองว่า </span>
             นาย/นาง/นางสาว <FiLL class="min-w-36" :data="`${data?.fName}  ${data?.lName}`"/> เป็นผู้มีคุณสมบัติ
             ในการสมัครสมาชิก สส.ชสอ. รวมทั้งได้แบบเอกสารหลักฐานประกอบการสมัคร และชำระเงินค่าสมัคร เงินบำรุง และ
@@ -33,7 +160,7 @@
         </div><br>
         <p class="pl-72">ตำเเหน่ง <FiLL :data="data?.position" class="min-w-28" /></p>
  </div>
- <div class=" grid justify-items-center mt-4">
+ <div class=" grid justify-items-center mt-2">
             <p class="mt-2 font-medium font-bold text-xl">สำหรับคณะกรรมการศูนย์ประสานงาน</p>
         </div>  
         <div class="grid justify-items-start  mt-4">

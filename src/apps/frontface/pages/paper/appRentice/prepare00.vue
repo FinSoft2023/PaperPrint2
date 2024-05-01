3cc08b3333cebbd939e4c32aabc36a4395a4c835
reregami Nonthawat <66309010023@loeitech.ac.th>
2024-05-01 15:20:58 +0700

    oo up

--- src/apps/frontface/pages/paper/appRentice/prepare00.vue
+++ src/apps/frontface/pages/paper/appRentice/prepare00.vue
@@ -71,46 +71,36 @@
        <p class="text-base"> <span class="ml-12">ข้าพเจ้า</span> (นางสาว/นาง/นาย) 
               <FiLL class="min-w-80" :data="`${data?.prefix} ${data?.fName}  ${data?.lName}`"/>
             อายุ <FiLL :data="data?.age" class="min-w-24" /> ปี เลขประจำตัวประชาชน <FiLL :data="data?.idcard" class="min-w-96" />
-            เกิดวันที่ <FiLL  class="min-w-56 " :data="data?.dateReceived" date-format="full" /> คู่สมรสชื่อ <FiLL class="min-w-96" :data="`${data?.newprefix} ${data?.newfName}  ${data?.lName}`"/>
+            เกิดวันที่ <FiLL  class="min-w-56 " :data="data?.dateReceived" date-format="full" /> 
           </p> 
-          <p class="text-base"> (1) <FiLLBox :checked="data?.savings"/> สมาชิกสามัญของสหกรณ์ออมทรัพย์ <FiLL :data="data?.savings" class="min-w-40" />จำกัด เลขสมาชิกสหกรณ์ <FiLL :data="data?.member" class="min-w-32" />
-          <span class="text-base ml-6"><FiLLBox /> อื่นๆ </span>(ระบุ).....................................................................................................
-          </p>
-          <p class="text-base"> (2) ตำแหน่ง <FiLL :data="data?.position" class="min-w-40" /> หรือปฏิบัติหน้าที่ <FiLL :data="data?.position" class="min-w-40" />
-      <br> <span class="text-base ml-6"> สถานที่ปฏิบัติงาน / ชื่อหน่วยงาน </span><FiLL :data="data?.agency" class="min-w-80" /> <br> <span class="text-base ml-6">ตำบล (แขวง)</span> <FiLL :data="data?.district" class="min-w-44" />
-      อำเภอ (เขต) <FiLL :data="data?.district" class="min-w-32" /> <br> <span class="text-base ml-6">จังหวัด</span> <FiLL :data="data?.province" class="min-w-32" /> รหัสไปรษณี <FiLL :data="data?.zipcode" class="min-w-32" />
-      <br> <span class="text-base ml-6">โทรศัพท์ (ที่ทำงาน)</span> <FiLL :data="data?.nbPhon" class="min-w-32" /> 
-    </p>
-<p class="text-base ">(3)  สถานที่อยู่(ในการจัดส่งเอกสาร) <br>
+
+        
+          <span class="text-base ml-12">1. เลขทะเบียนสมาชิก กสรท. ที่...........</span>
+          <span class="text-base ml-6"><FiLLBox /> สหกรณ์ออมทรัพย์........จำกัด</span>
+          <span class="text-base ml-6"><FiLLBox /> สมัครตรง กสธท.........</span>
+          <br>
+          <span class="text-base ml-6"><FiLLBox /> เป็นสมาชิกสามัญ สสธท. และ กลธท. ด้านที่ 2</span>
+          <span class="text-base ml-6"><FiLLBox /> เป็นเจ้าหน้าที่ กสธท./สสธท</span>
+          <br>
+          <span class="text-base ml-6"><FiLLBox /> เป็นกรรมการ/ผู้ตรวจสอบกิจการ/เจ้าหน้าที่ ชสอ.</span>
+          <br>
+          <span class="text-base ml-6"><FiLLBox /> เป็นสมาชิกสมทบ สสธท. และ กสชท. ล้านที่ 2 (ประเภทสมทบสหกรณ์ออมทรัพย์) ที่เป็นพนักงานราชการ/พกส./ 
+                                                   ลูกจ้างชั่วคราว ในสังกัดกระทรวงสาธารณสุข</span>
+          <br>
+          <span class="text-base ml-6"><FiLLBox /> เป็นสมาชิกสมทบคู่สมรส/สมทบบุตร สสชท, และ กลธท. ด้านที่ 2 ที่ประกอบอาชีพ (โปรดระบุ)..........<br>ยกเว้นอาชีพเกษตรกร และผู้ไม่ได้ประกอบอาชีพ</span>
+
+          <br><span class="text-base ml-12">2. สถานที่อยู่ (ในการจัดส่งเอกสาร)</span>
 <span class="text-base ml-6">บ้านเลขที่ </span><FiLL :data="data?.housenb" class="min-w-32" /> หมู่ที่ <FiLL :data="data?.group" class="min-w-32" /> ซอย .................... ถนน <FiLL :data="data?.road" class="min-w-32" />
 <br> <span class="text-base ml-6">ตำบล (แขวง)</span> <FiLL :data="data?.district" class="min-w-32" />
       อำเภอ (เขต) <FiLL :data="data?.district" class="min-w-32" /> จังหวัด <FiLL :data="data?.province" class="min-w-32" /><br>  <span class="text-base ml-6">รหัสไปรษณี</span> <FiLL :data="data?.zipcode" class="min-w-32" />
-      โทรศัพท์ (บ้าน) <FiLL :data="data?.nbPhon" class="min-w-32" />  โทรศัพท์ (มือถือ) <FiLL :data="data?.nbPhon" class="min-w-32" /> 
-</p> 
-<p class="text-base ">(4) การชำระเงิน ดังนี้ <br>
-<span class="text-base ml-12">4.1การชำระเงินครั้งแรก</span>
+      โทรศัพท์ <FiLL :data="data?.nbPhon" class="min-w-32" />  โทรศัพท์ <FiLL :data="data?.nbPhon" class="min-w-32" /> 
+
 <br>
-<div class="grid grid-rows-2 grid-flow-col">
-                <div class="row-span-3">
-                  <p class="ml-12" ><FiLLBox :checked="data?.savings"/> ค่าสมัครสมาชิกครั้งแรก</p>
-                  <p class="ml-12"> <FiLLBox :checked="data?.savings"/> ค่าบำรุงรายปี </p>
-                 
-                </div>
-                <div class="row-span-2 col-span-2 ml-10">
-                  <p>เป็นเงิน 20 บาท</p>
-                  <p>เป็นเงิน 20 บาท</p>                  
-                </div>
-            </div>
-            <span class="text-base ml-12"><FiLLBox :checked="data?.savings"/> เงินสงเคราะห์ล่วงหน้า..............5,500.....บาท</span> <br>
-            <span class="text-base ml-20">รวมชำระหนี้ครั้งนี้ เป็นเงิน..........5,500.....บาท</span> (ห้าพันห้าร้อยยี่สิบบาทถ้วน)<br>
-            <span class="text-base ml-12">4.2 การชำระเงินครั้งต่อไป ข้าพเจ้ายินยอมให้หักและชำระเงินสงเคราะห์ล่วงหน้า ดังน้ี</span><br>
-            <span class="text-base ml-12"> <FiLLBox :checked="data?.savings"/> ชำเป็นเงินสดที่สมาคมหรือศุนย์ประสานงานสมาคม</span><br>
-            <span class="text-base ml-12"> <FiLLBox :checked="data?.savings"/> ชำระเงินเข้าบัญชีสมาคมธนาคารไทยพาณิชย์ สาขาเดอะคริสตัล ราชพฤกษ์ ชื่อบัญชี สมาคมฌาปนกิจ</span>สงเคราะห์สมาชิกสหกรณ์ออมทรัพย์สาธารณสุขไทย
-            สมาคมฌาปนกิจสงเคราะห์สมาชิกสหกรณ์ออมทรัพย์สาธารณสุขไทย ประเภทกระแสรายวัน (Bill Payment Pay- In Slip) เลขบัญชี 458-0-39888-5<br>
-            <span class="text-base ml-12"> <FiLLBox :checked="data?.savings"/> ชำระจากเงินปันผล เฉลี่ยคืน จากสหกรณ์ต้นสังกัด <br>
-              <span class="text-base ml-12"> ข้าพเจ้าได้ทราบและเข้าใจวัตถุประสงค์ </span> ข้อบังคับ ระเบียบ และประกาศของสมาคมฌาปนกิจสงเคราะห์</span>
-            สมาชิกสามัญของสหกรณ์ออมทรัพย์สาธารณสุขไทย แล้ว มีความประสงค์สมัครเป็นสมาชิก และยินยอมปฏิบัติตามข้อบังคับและเงื่อไขดังกล่าวทุกประการ<br>
-</p> 
+<span class="text-base ml-12"> 3. ขอสมัครเป็นสมาชิกของกองทุนสวัสดิการสมาชิกของสหกรณ์ออมทรัพย์สาธารณสุขไทย (กสธท.) ด้านที่ 3</span>
+<br>
+<span class="text-base ml-12"> 4. การชำระเงินค่าสมัครและเบี้ยประกันชีวิต ดังนี้</span>
+                
+
 <p class="text-base pl-96 ">ลงชื่อ...........................................................ผู้สมัคร </p>
 <p class="text-base pl-96 ">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;( <FiLL class="min-w-52" :data="`${data?.prefix} ${data?.fName}  ${data?.lName}`"/> )</p>
 

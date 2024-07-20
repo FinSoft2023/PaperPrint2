54b2245ce167ee312a7393ee2f10f2a223d0b90b
Jane1m <rinda@finsoft2023.com>
2024-07-20 19:41:50 +0700

    add withdrawalSlip add depositSlip

--- src/apps/frontface/pages/paper/depositSlip.vue
+++ src/apps/frontface/pages/paper/depositSlip.vue
@@ -1,86 +1,63 @@
 <template>
     <main>
-        <PaperDoc>
-            <!-- <div class="border-t border-r border-l border-b border-gray-900 p-2">
-            </div> -->
-            <div class="font-bold text-md mt-2">ใบส่งเงินฝากออมทรัพย์พิเศษ SPECIAL SAVVINGS DEPOSIT SLIP</div>
-            <div class="flex justify-end mt-3">
-                <p>สหกรณ์ออมทรัพย์.........................................................................................จำกัด
-                </p>
+        <PaperDoc>           
+            <div class="font-bold text-md mt-2 px-8 text-gray-100 bg-gray-800 dark:bg-gray-700 dark:text-white">ใบส่งเงินฝากออมทรัพย์พิเศษ SPECIAL SAVINGS DEPOSIT SLIP</div>
+            <div class="flex justify-end mt-3 text-gray-900 dark:text-gray-900">
+                <p class="px-4 ">สหกรณ์ออมทรัพย์ </p> <FiLL :data="data?.location" class="underline underline-offset-4 text-base " no-line /> 
+                <p class="px-4">จำกัด</p>
+               
             </div>
-            <hr class="my-4 border-b-2 border-black">
-            <div class="grid grid-cols-2">
-                <div>
+            <hr class="my-2 border-b-2 border-black">
+            <div class="grid grid-cols-2 dark:text-gray-900">
+                <div class="text-sm">
                     <p>สำนักงาน</p>
                     <p>Office</p>
-                    <hr class="h-px w-48 my-1 bg-gray-200 border-0 dark:bg-gray-700">
+                    <hr class="h-px my-1 bg-gray-200 border-0 dark:bg-gray-700">
                     <p>วันที่</p>
                     <p>Date</p>
                 </div>
-                <div class="flex flex-col gap-1">                   
-                    <div class="flex items-center">
-                        <input id="default-radio-1"
-                               type="radio"
-                               value=""
-                               name="default-radio"
-                               class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                        <label for="default-radio-1"
-                               class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">เงินสด Cash
-                        </label>
+                <div class="flex flex-col ml-2">                   
+                    <div class="flex items-center">                        
+                        <div class="flex items-center"><FiLLBox />
+                            <p class="ml-2">เงินสด Cash</p> 
+                        </div>
                     </div>
-                    <div class="flex items-center">
-                        <input id="default-radio-1"
-                               type="radio"
-                               value=""
-                               name="default-radio"
-                               class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                        <label for="default-radio-1"
-                               class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">รายกานโอน Transfer
-                        </label>
+                    <div class="flex items-center">                        
+                        <div class="flex items-center"><FiLLBox />
+                            <p class="ml-2">รายกานโอน Transfer</p> 
+                        </div>
                     </div>
-                    <div class="flex items-center">
-                        <input id="default-radio-1"
-                               type="radio"
-                               value=""
-                               name="default-radio"
-                               class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                        <label for="default-radio-1"
-                               class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">เช็คเรียกเก็บ CL/CC
-                        </label>
+                    <div class="flex items-center">                        
+                        <div class="flex items-center"><FiLLBox />
+                            <p class="ml-2">เช็คเรียกเก็บ CL/CC</p> 
+                        </div>
                     </div>
-                    <div class="flex items-center mb-2">
-                        <input id="default-radio-1"
-                               type="radio"
-                               value=""
-                               name="default-radio"
-                               class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                        <label for="default-radio-1"
-                               class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">อื่น ๆ Others
-                        </label>
-                    </div>
-
+                    <div class="flex items-center">                        
+                        <div class="flex items-center"><FiLLBox />
+                            <p class="ml-2">อื่น ๆ Others</p> 
+                        </div>
+                    </div>                  
                 </div>
             </div>
             <table class="w-full text-xs text-left text-gray-500 dark:text-gray-400 border border-black">
                 <tbody>
-                    <tr class="bg-white dark:bg-gray-800 ">
+                    <tr class="bg-gray-300 dark:bg-gray-400 dark:text-gray-400">
                         <td scope="row"
                             colspan="2"
-                            class="px-6 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 text-xs font-bold text-gray-900 whitespace-nowrap  dark:text-gray-900 border-r border-b border-black">
                             <div class="flex">
                                 ชื่อบัญชี Account Name
-                                <p class="ml-8">
-                                    <FiLL :data="data?.fname" />
-                                    <FiLL :data="data?.lname" />
+                                <p class="ml-2">
+                                <FiLL class="min-w-56" :data="`${data?.fName}  ${data?.lName}`"/>
                                 </p>
                             </div>
                         </td>
 
                         <td scope="row"
-                            class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
                             <div class="flex">
                                 เลขที่บัญชี Account No.
-                                <p class="ml-8">
+                                <p class="ml-2">
                                     6895420004
                                 </p>
                             </div>
@@ -88,133 +65,135 @@
 
                         </td>
                         <td scope="row"
-                            class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
-
+                            class="px-2 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
                         </td>
 
                     </tr>
-                    <tr class="bg-white dark:bg-gray-800 ">
+                    <tr class="bg-white dark:bg-white ">
                         <td scope="row"
                             colspan="2"
-                            class="px-6 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
                             ยอดเงินสดนำฝาก Amount in Cash Deposit
                         </td>
                         <td scope="row"
-                            class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
                             <p>จำนวนเงิน Amount in Baht</p>
 
                         </td>
                         <td scope="row"
                             rowspan="4"
-                            class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="align-top px-2 py-1 pb-6 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
                             <p class="flex justify-center">ลายมือชื่อผู้นำฝาก</p>
                             <p class="flex justify-center">Deposit By</p>
                         </td>
                     </tr>
-                    <tr class="bg-white dark:bg-gray-800 ">
+                    <tr class="bg-white dark:bg-white ">
                         <td scope="row"
                             colspan="2"
-                            class="px-6 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
-                            <FiLL :data="data?.salary" class="min-w-36" /> 
+                            class="px-2 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
+                            <FiLL :data="data?.salary" class="min-w-28" />
                         </td>
                         <td scope="row"
-                            class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
-                            <FiLL :data="data?.oldInterestRate" class="min-w-16" /> 
+                            class="px-2 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
+                            <FiLL :data="data?.salary" class="min-w-28" />
                         </td>
                     </tr>
-                    <tr class="bg-white dark:bg-gray-800 ">
+                    <tr class="text-gray-100 bg-gray-300 dark:bg-gray-400 dark:text-gray-400">
                         <td scope="row"
-                            class="px-6 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
                             ธนาคาร/สาขา Bank/Branch
                         </td>
                         <td scope="row"
-                            class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
                             <p>เลขที่เช็ค Cheque Number</p>
 
                         </td>
                         <td scope="row"
-                            class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
                         </td>
                     </tr>
-                    <tr class="bg-white dark:bg-gray-800 ">
+                    <tr class="bg-white dark:bg-white ">
                         <td scope="row"
-                            class="px-6 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
 
                         </td>
                         <td scope="row"
-                            class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
                             <p></p>
 
                         </td>
                         <td scope="row"
-                            class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
                             .
                         </td>
                     </tr>
-                    <tr class="bg-white dark:bg-gray-800 ">
+                    <tr class="bg-white dark:bg-white ">
                         <td scope="row"
-                            class="px-6 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
 
                         </td>
                         <td scope="row"
-                            class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
                             <p></p>
 
                         </td>
                         <td scope="row"
-                            class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
                             .
                         </td>
                         <td scope="row"
                             rowspan="4"
-                            class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="align-top px-2 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
                             <p class="flex justify-center">ลายมือชื่อเจ้าหน้าที่</p>
                             <p class="flex justify-center">Authorized Signature</p>
                         </td>
                     </tr>
-                    <tr class="bg-white dark:bg-gray-800 ">
+                    <tr class="bg-white dark:bg-white ">
                         <td scope="row"
-                            class="px-6 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
 
                         </td>
                         <td scope="row"
-                            class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
                             <p></p>
 
                         </td>
                         <td scope="row"
-                            class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
                             .
                         </td>
                     </tr>
-                    <tr class="bg-white dark:bg-gray-800 ">
+                    <tr class="text-gray-100 bg-gray-300 dark:bg-gray-400 dark:text-gray-400 ">
                         <td scope="row"
                             colspan="2"
-                            class="px-6 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
                             ยอดรวมเงินฝากเป็นตัวอักษร Total Amount in Words
                         </td>
 
                         <td scope="row"
-                            class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
                             ยอดรวมเงินฝาก Total Amount
                         </td>
                     </tr>
-                    <tr class="bg-white dark:bg-gray-800 ">
+                    <tr class="bg-white dark:bg-white ">
                         <td scope="row"
                             colspan="2"
-                            class="px-6 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
+                            class="px-2 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
+                            <MoneyText :data="data?.monthlyIncome" />
                         </td>
                         <td scope="row"
-                            class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-white border-r border-b border-black">
-                            .
+                            class="px-2 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
+                            <FiLL :data="data?.salary" class="min-w-28" />
                         </td>
                     </tr>
                 </tbody>
             </table>
-            <div class="text-xs mt-2">
+            <div class="text-xs mt-2 text-gray-ต00 dark:text-gray-900">
                 <div class="font-bold">
                 </div>
-                <div class="flex">หมายเหตุ <p class="ml-2">1. โปรดทำใบนำฝากแยกต่างหากอย่างละฉบับ สำหรับเงินสด
+                <div class="flex">
+                    <p class="underline">หมายเหตุ</p>                    
+                     <p class="ml-2">1. โปรดทำใบนำฝากแยกต่างหากอย่างละฉบับ สำหรับเงินสด
                         และเช็คหรือดร๊าฟท์
                         2. โปรดเขียนชื่อหรือหมายเลขบัญชีหลังเช็คหรือดร๊าฟท์ก่อนนำเข้าบัญชี</p>
                 </div>
@@ -233,5 +212,5 @@
         lang="ts">
         import Index from '../index.vue';
 
-        const { data } = await useFetch<any>('/api/loanEmergency');
+        const { data } = await useFetch<any>('/api/group2');
 </script>
---
+++ src/apps/frontface/pages/paper/withdrawalSlip.vue
@@ -0,0 +1,146 @@
+<template>
+    <main>
+        <PaperDoc>
+            <div class="font-bold text-md mt-2 px-8 text-gray-100 bg-gray-800 dark:bg-gray-700 dark:text-white">ใบถอนเงินฝากออมทรัพย์พิเศษ SPECIAL SAVINGS WITHDRAWAL SLIP
+               </div>
+            <div class="flex justify-end mt-3 text-gray-900 dark:text-gray-900">
+                <p class="px-4 ">สหกรณ์ออมทรัพย์ </p> <FiLL :data="data?.location" class="underline underline-offset-4 text-base " no-line /> 
+                <p class="px-4">จำกัด</p>
+               
+            </div>
+            <hr class="my-2 border-b-2 border-black">
+            <div class="grid grid-cols-2 dark:text-gray-900">
+                <div class="text-sm">
+                    <p>สำนักงาน</p>
+                    <p>Office</p>
+                    <hr class="h-px my-1 bg-gray-200 border-0 dark:bg-gray-700">
+                    <p>วันที่</p>
+                    <p>Date</p>
+                </div>
+            </div>
+
+            <div class="">
+                <table class="w-full text-xs text-left text-gray-500 dark:text-gray-400 border border-black">
+                    <tbody>
+                        <tr class=" bg-gray-300 dark:bg-gray-400 dark:text-gray-400 ">
+                            <td scope="row"
+                                colspan="4"
+                                class="px-6 py-1 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
+                                <div class="flex">
+                                ชื่อบัญชี Account Name
+                                <p class="ml-2">
+                                <FiLL class="min-w-56" :data="`${data?.fName}  ${data?.lName}`"/>
+                                </p>
+                            </div>
+                            </td>
+                            <td scope="row"
+                                colspan="3"
+                                class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
+                                <div class="flex">
+                                    เลขที่บัญชี Account No.
+                                    <p class="ml-8">
+                                        6895420004
+                                    </p>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="bg-white dark:bg-white ">
+                            <td scope="row"
+                                rowspan="1"
+                                colspan="3"
+                                class="text-xs font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
+                                <p> จำนวนเงินที่ถอนเป็นตัวอักษร Withdrewal Amount in Words </p>
+                                <p class="flex justify-center">
+                                    <MoneyText :data="data?.monthlyIncome" />
+                                </p>
+                            </td>
+                            <td scope="row"
+                                class="py-1 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
+                                <p class="ml-8"> บาท </p>
+                                <p class="ml-8"> Baht </p>
+                            </td>
+                            <td scope="row"
+                                colspan=""
+                                class="px-6 py-1 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
+                                <p> จำนวนเงินที่ถอน Withdrewal Amount</p>
+                                <p class="flex justify-center">
+                                    <FiLL :data="data?.salary"
+                                          class="min-w-36" />
+                                </p>
+                            </td>
+                        </tr>
+                        <tr class="bg-white dark:bg-white ">
+                            <td scope="row"
+                                colspan=""
+                                class="py-1 pb-6 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
+                                ลายมือชื่อเจ้าของบัญชี Account Holder's Signature
+                            </td>
+                            <td scope="row"
+                                colspan="4"
+                                class="py-1 pb-6 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
+                                <p>ลายมือชื่อ ผู้รับเงิน (ผู้รับมอบฉันทะ)</p>
+                                <p>Receiver's Signature (Nominee)</p>
+                            </td>
+                        </tr>
+                        <tr class="bg-white dark:bg-white ">
+                            <td scope="row"
+                                colspan="4"
+                                class="py-1 pb-6 text-xs font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
+                                สำหรับเจ้าหน้าที่
+                            </td>
+                            <td scope="row"
+                                colspan=""
+                                class="py-1 pb-6 font-bold text-gray-900 whitespace-nowrap dark:text-gray-900 border-r border-b border-black">
+                                ลายมือชื่อเจ้าหน้าที่ Authorized Signature
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+
+            </div>
+        </PaperDoc>
+        <MorePaperDoc>
+            <p class="font-bold text-xl text-center col-span-2">ใบมอบฉันทะ</p>
+      <div class="flex flex-col items-end mt-3 mb-6">
+        <p>วันที่............................................................</p>        
+      </div>     
+      <div class="flex flex-col">
+        <p>
+          ข้าพเจ้า....................................................................................เจ้าของบัญชีเลขที่..................................................................................
+        </p>
+        <p>
+            ขอมอบฉันทะให้...................................................<FiLLBox /> บัตรประจำตัวประชาชน  <FiLLBox />บัตรข้าราชการ เลขที่...................................................
+        </p>
+        <p>
+            ออกโดย.......................................................................วันที่ออก..................................................................วันหมดอายุ...................................
+        </p>
+        <p>
+            ที่อยู่ เลขที...............................ถนน........................ตำบล/แขวง...........................อำเภอ/เขต....................จังหวัด..................................
+        </p>
+        <p>เป็นผู้รับเงินจำนวนที่ขอถอน และรับสมุดคู่ฝากของข้าพเจ้าคืนจากสหกรณ์ฯ และในการนี้ข้าพเจ้าเป็นผู้รับผิดชอบเสมือนหนึ่งข้าพเจ้าได้กระทำด้วยตนเองทั้งสิ้น</p>
+        <div class="mt-16 flex justify-center gap-4 pl-80">
+            <div>
+                <p class="text-base text-center">...................................................</p>
+                <p class="text-base text-center">ลายมือชื่อเจ้าของบัญชี</p>
+            </div>
+            <div>
+                <p class="text-base text-center">...................................................</p>
+                <p class="text-base text-center">ลายมือชื่อผู้รับมอบฉันทะ</p>
+                <p class="text-base text-center">(ต้องเซ็นต่อหน้าเจ้าของบัญชี)</p>
+            </div>
+        </div>
+
+      </div>    
+        </MorePaperDoc>
+
+
+    </main>
+</template>
+
+<script setup
+        lang="ts">
+        import Index from '../index.vue';
+
+        const { data } = await useFetch<any>('/api/group2');
+</script>
